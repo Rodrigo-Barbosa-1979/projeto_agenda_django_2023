3ecc68dae9859412829d20f41494febcd89e2820
Rodrigo Barbosa <barbosa.rodrigo.1979@gmail.com>
2023-12-12 05:13:44 -0300

    Update on 12-12-23

--- contact/forms.py
+++ contact/forms.py
@@ -1,22 +1,87 @@
+from collections.abc import Mapping
+from typing import Any
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
 from contact.models import Contact
 from django import forms
 from django.core.exceptions import ValidationError
 
 
 class ContactForm(forms.ModelForm):
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "classe-a classe-b",
+                "placeholder": "Escreva aqui",
+            }
+        ),
+        label="Primeiro nome",
+        help_text="Texto de ajuda para seu usuário",
+    )
+
+    # def __init__(self, *args, **Kwargs):
+    #     super().__init__(*args, **Kwargs)
+
+    # self.fields["first_name"].widget.attrs.update({
+    #     "class": "classe-a classe-b",
+    #     "placeholder": "Escreva aqui",
+    # })
+
     class Meta:
         model = Contact
-        fields = "first_name", "last_name", "phone",
+        fields = "first_name", "last_name", "phone", "email", "description", "category",
+
+        # widgets = {
+        #     "first_name": forms.TextInput(
+        #         attrs={
+        #             "class": "classe-a classe-b",
+        #             "placeholder": "Escreva aqui",
+        #         }
+        #     )
+        # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
 
-        self.add_error(
-            "first_name",
-            ValidationError(
-                "Mensagem de erro",
-                code="invalid",
-            )
-        )
+        if first_name == last_name:
+            msg = "Primeiro nome não pode ser igual ao segundo nome."
+
+            self.add_error("first_name", msg)
+            self.add_error("last_name", msg)
+
+        # self.add_error(
+        #     "first_name",
+        #     ValidationError(
+        #         "Mensagem de erro",
+        #         code="invalid",
+        #     )
+        # )
+
+        # self.add_error(
+        #     "first_name",
+        #     ValidationError(
+        #         "Mensagem de erro 2",
+        #         code="invalid",
+        #     )
+        # )
 
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get("first_name")
+
+        # raise ValidationError(
+        #     "Mensagem do raise.",
+        #     code="invalid",
+        # )
+
+        if first_name == "ABC":
+            raise ValidationError(
+                "ABC não é válido neste campo."
+            )
+
+        return first_name
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from django.urls import reverse
 
 from contact.forms import ContactForm
+from contact.models import Contact
 
 
 def create(request):
+    form_action = reverse("contact:create")
 
     if request.method == "POST":
+        form = ContactForm(request.POST)
+
         context = {
-            "form": ContactForm(request.POST)
+            "form": form,
+            "form_action": form_action,
         }
+
+        if form.is_valid():
+            contact = form.save()
+
+            # contact = form.save(commit=False)
+            # contact.show = False
+            # contact.save()
+
+            return redirect("contact:update", contact_id=contact.id)
 
         return render(
             request,
@@ -17,7 +32,49 @@ def create(request):
         )
 
     context = {
-        "form": ContactForm()
+        "form": ContactForm(),
+        "form_action": form_action,
+    }
+
+    return render(
+        request,
+        "contact/create.html",
+        context,
+    )
+
+
+def update(request, contact_id):
+    contact = get_list_or_404(Contact, id=contact_id, show=True)
+    form_action = reverse("contact:update", args=(contact_id,))
+
+    print(f'Contact: {contact}')
+
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+
+        context = {
+            "form": form,
+            "form_action": form_action,
+        }
+
+        if form.is_valid():
+            contact = form.save()
+
+            # contact = form.save(commit=False)
+            # contact.show = False
+            # contact.save()
+
+            return redirect("contact:update", contact_id=contact.id)
+
+        return render(
+            request,
+            "contact/create.html",
+            context,
+        )
+
+    context = {
+        "form": ContactForm(instance=contact),
+        "form_action": form_action,
     }
 
     return render(
